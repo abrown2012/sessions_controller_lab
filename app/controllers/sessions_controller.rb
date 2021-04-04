2f1fb575fa7a73588f7cad744965643a5f6b455b
Agnieszka Brown <abrown2012@icloud.com>
2021-04-03 21:11:05 -0400

    commit tree f98249ba81a31f78d0465d3db1ffa83ec72be908

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ActionController::Base
+  def new
+    # nothing to do here!
+  end
+
+  def create
+    if params[:name] == nil || params[:name] == ""
+      redirect_to '/login'
+    else 
+      session[:name] = params[:name]
+      redirect_to '/'
+    end 
+  end
+
+  def destroy
+    session.delete :name
+  end 
+
+end
